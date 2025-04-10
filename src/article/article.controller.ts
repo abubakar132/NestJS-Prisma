7650732244857965b0064e3b63002b9e5c2c02af
Abu Bakar <abubakarrr132@gmail.com>
2025-04-10 19:43:05 +0500

    Added Input Validations - Part 2 of tutorial

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
@@ -22,21 +22,18 @@ export class ArticleController {
     return this.articleService.findDrafts();
   }
 
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.articleService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.articleService.findOne(id);
   }
-  
-
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articleService.update(+id, updateArticleDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articleService.update(id, updateArticleDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.articleService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.articleService.remove(id);
   }
 }
